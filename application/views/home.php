4e24ec7aa4377ebff2f63e0be0b1ac41428773f7
JsOliver <joaovictordada2@gmail.com>
2017-01-24 13:02:46 -0200

    Garantia de Qualidade

--- application/views/home.php
+++ application/views/home.php
@@ -218,9 +218,7 @@ endif;
                 <div class="col-md-6 col-sm-7">
                     <div class="home-wrapper home-wrapper-alt p-0">
                         <h1 class="h1 font-light w-full" style="text-shadow: 1px 1px 5px #ffffff; color:#141f3d;">QUEM SOMOS</h1>
-                        <h4 class="text-light w-full" style="text-shadow: 1px 1px 5px #ffffff;  color:#141f3d;">Somos a primeira empresa especializada na comercialização de pescados POR LEILÃO REVERSO OU LEILÃO DIGITAL no Brasil.
-                            COM BASE no sistema adotado pela Cooperativa de Produtores de Flores de Holambra OU VEILING® HOLAMBRA criamos a Aquamercantil. 
-                            Uma ferramenta para facilitar o comércio de pescados e aproximar o seu produtor do mercado.
+                        <h4 class="text-light w-full" style="text-shadow: 1px 1px 5px #ffffff;  color:#141f3d;"><b>Somos a primeira empresa especializada na comercialização de pescado por meio de leilão no Brasil. Com base no sistema adotado pelas cooperativas de produtores de flores em Holambra - VEILING® HOLAMBRA, criamos a Aqua Mercantil um sistema de Leilão Reverso On Line. Uma ferramenta para facilitar o comércio de pescados e aproximar o seu produto do mercado.</b>
 </h4>
 
                     </div>
@@ -2677,7 +2675,7 @@ endif;
                     <div class="col-sm-12 text-center">
                         <div class="home-wrapper home-wrapper-alt p-1">
                             <h1 class="" style=" color:#141f3d;">QUEM SOMOS</h1>
-                            <h4 class="text-light" style="text-shadow: 1px 1px 5px #ffffff; color:#141f3d;">Somos a primeira empresa especializada na comercialização de pescados no Brasil. Baseados no sistema adotado pela cooperativa de Holambra criamos a Aquamercantil. Uma ferramenta para facilitar o comércio de pescados e aproximar o produtor do mercado.</h4>
+                            <h4 class="text-light" style="text-shadow: 1px 1px 5px #ffffff; color:#141f3d;"><b>Somos a primeira empresa especializada na comercialização de pescado por meio de leilão no Brasil. Com base no sistema adotado pelas cooperativas de produtores de flores em Holambra - VEILING® HOLAMBRA, criamos a Aqua Mercantil um sistema de Leilão Reverso On Line. Uma ferramenta para facilitar o comércio de pescados e aproximar o seu produto do mercado.</b></h4>
                             <a href="<?php echo base_url('quem-somos');?>" class="btn btn-white-bordered" style="color:rgba(20, 31, 61, 0.84); ">Ver mais</a>
 
                         </div>
@@ -4041,8 +4039,96 @@ if(!isset($_SESSION['ID'])):
 
     </div> <!-- end container -->
 </section>
-
+</section>
 <?php endif;?>
+
+<div class="col-sm-12 text-center">
+    <h3 class="title">Garantia de Qualidade</h3>
+
+</div>
+<section class="section" id="gqualidade">
+
+    <div class="row wide-img-showcase-row"  >
+        <div class="col-sm-6 no-padding img" style="background-image: url(http://www.anda.jor.br/wp-content/uploads/2011/02/a14.jpg)">
+        </div>
+        <!-- end col -->
+        <div class="col-sm-6 col-sm-offset-6 bg-light">
+            <div class="about-detail-img-box">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="text-center">
+                            <div class="title-box-icon title-about">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <div class="feat-description text-center">
+                            <h4 class="text-muted">•	Transparência e igualdade nas negociações;<br>
+
+                                •	Produtos frescos com menos custos operacionais;<br>
+                                •	Novo conceito na comercialização de pescados baseado na oferta e procura;<br>
+                                •	Assessoria em marketing e promoção de vendas;<br>
+                                •	Atuação em todos os segmentos do mercado;<br>
+                                •	Garantia de qualidade;<br>
+                                •	Garantia de pagamento;<br>
+                                •	Ampla carteira de clientes;<br>
+                                •	Controle na padronização dos produtos;<br>
+                                •	Informação do mercado para auxílio das decisões;<br>
+                            </h4>
+
+
+                        </div><!-- Features Description -->
+                    </div> <!-- end col -->
+                </div> <!-- end row -->
+            </div> <!-- end about detail -->
+        </div>
+
+    </div>
+    <section>
+        <div class="row margin-0 wide-img-showcase-row">
+
+            <div class="col-sm-6"  style="background: #2f3e47;">
+                <div class="about-detail-img-box" >
+
+                    <div class="row" >
+                        <div class="col-sm-12">
+                            <div class="text-center">
+                                <div class="title-box-icon title-about text-white">
+                                    <h3 class="title">
+                                        GARANTIA DE QUALIDADE AQUAMERCANTIL
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- end row -->
+
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2">
+
+                            <div class="feat-description text-center">
+                                <h4 class="text-light">Os produtos ofertados nos leilões da Aquamercantil, antes de serem enviados ao comprador, são avaliados por engenheiros de pesca que atestam a qualidade, a sanidade e as características do produto. É realizada a biometria do produto, garantindo dessa forma que não haja risco de envio de produtos que não estejam de acordo com as características especificadas.
+                                    Neste caso, a liberação dos pagamentos e o envio do produto só ocorre após a emissão do laudo pela Aquamercantil, isso garante o sucesso do negócio para todos.
+                                </h4>
+
+                            </div><!-- Features Description -->
+
+                        </div> <!-- end col -->
+                    </div> <!-- end row -->
+                </div> <!-- end about detail -->
+            </div> <!-- end col -->
+
+            <div class="col-sm-6 col-sm-offset-6 no-padding img-2 " style="background-image: url(http://s2.glbimg.com/9q7NiQ2TtjEoJTr1-TYi3Ex-KnE=/780x440/e.glbimg.com/og/ed/f/original/2015/12/10/peixes-pesca-pescaria-piscultura-bill-bredley.jpg);">
+            </div>
+
+
+        </div> <!-- end row -->
+
+    </section>
+</section>
 <section class="section bg-light" id="patrocinadores">
     <div class="container">
         <div class="row">
